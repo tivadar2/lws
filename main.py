1951ee1a9ec4a08c272014c8a1f2f2556c8ababb
tivadar2 <pongo.tivadar@gamil.com>
2015-08-05 23:33:58 +0200

    Making GUI
    Added two tabs, add sentences and language, it will still change.

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ from main_ui import Ui_MainWindow
 import sys
 
 class LwsGui(Ui_MainWindow):
-    def __init__(self):
+    def __init__(self, MainWindow):
         Ui_MainWindow.__init__(self)
+        self.setupUi(MainWindow)
+        self.pushButton.clicked.connect(self.printsome)
+
+    def printsome(self):
+        print(self.textEdit.toPlainText())
     
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = LwsGui()
-    ui.setupUi(MainWindow)
+    ui = LwsGui(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
