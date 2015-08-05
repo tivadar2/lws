145040267ccc83bbab0447eec3435c8ed1bb3acd
tivadar2 <pongo.tivadar@gamil.com>
2015-08-06 00:01:45 +0200

    Added TinyDB

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from main_ui import Ui_MainWindow
+from tinydb import TinyDB, where
 import sys
 
-class LwsGui(Ui_MainWindow):
+class LwsMain(Ui_MainWindow):
     def __init__(self, MainWindow):
         Ui_MainWindow.__init__(self)
         self.setupUi(MainWindow)
+
+        #bindings
         self.pushButton.clicked.connect(self.printsome)
+        self.pushButton_addLang.clicked.connect(self.addLanguage)
 
     def printsome(self):
+        db.insert({'van': 1})
         print(self.textEdit.toPlainText())
+
+    def addLanguage(self):
+        db.insert({'language': self.textEdit_language.toPlainText()})
+        print('Language has been added.')
     
+db = TinyDB('data/languages.json')
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = LwsGui(MainWindow)
+    ui = LwsMain(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
